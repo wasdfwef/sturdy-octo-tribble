806436ddb4ef6e3de8d171e76ed117dbbe814781
wasdfwef <128443504+wasdfwef@users.noreply.github.com>
2023-08-08 16:01:50 +0800

    Add files via upload

--- project_1/src/change_bmp.c
+++ project_1/src/change_bmp.c
@@ -66,27 +66,113 @@ int is_food_clicked(void)
 	}
 }
 
+//定义一个函数，用于判断用户是否点击了"餐具"对应的区域
+int is_tableware_clicked(void)
+{
+	struct touch_pos pos = get_touch_pos(); //获取用户的触摸坐标
+	
+	//判断用户的触摸坐标是否在"餐具"区域内
+	if (pos.x >= 0 && pos.x <= 100 && pos.y >= 120 && pos.y <= 220)
+	{
+		return 1; //返回1表示点击了"餐具"
+	}
+	else
+	{
+		return 0; //返回0表示没有点击"餐具"
+	}
+}
+
+//定义一个函数，用于判断用户是否点击了"用具"对应的区域
+int is_tool_clicked(void)
+{
+	struct touch_pos pos = get_touch_pos(); //获取用户的触摸坐标
+	
+	//判断用户的触摸坐标是否在"用具"区域内
+	if (pos.x >= 0 && pos.x <= 100 && pos.y >= 240 && pos.y <= 340)
+	{
+		return 1; //返回1表示点击了"用具"
+	}
+	else
+	{
+		return 0; //返回0表示没有点击"用具"
+	}
+}
+
+//定义一个函数，用于判断用户是否点击了"结算"对应的区域
+int is_checkout_clicked(void)
+{
+	struct touch_pos pos = get_touch_pos(); //获取用户的触摸坐标
+	
+	//判断用户的触摸坐标是否在"结算"区域内
+	if (pos.x >= 560 && pos.x <= 760 && pos.y >= 400 && pos.y <= 480)
+	{
+		return 1; //返回1表示点击了"结算"
+	}
+	else
+	{
+		return 0; //返回0表示没有点击"结算"
+	}
+}
+
 //定义一个函数，用于根据用户的触摸坐标切换不同的bmp图片
 void change_bmp(void)
 {
     //定义一个变量，用于存储当前显示的图片名称
-    char *current_bmp = "./1.bmp";
+    char *current_bmp = "./菜单.bmp";
 
     while (1) //无限循环
     {
         if (is_food_clicked()) //如果用户点击了"杂食"
         {
-            if (strcmp(current_bmp, "./1.bmp") == 0) //如果当前显示的是菜单.bmp
+            if (strcmp(current_bmp, "./菜单.bmp") == 0) //如果当前显示的是菜单.bmp
             {
-                show_bmp("./2.bmp"); //显示杂食.bmp
-                current_bmp = "./2.bmp"; //更新当前显示的图片名称
+                show_bmp("./杂食.bmp"); //显示杂食.bmp
+                current_bmp = "./杂食.bmp"; //更新当前显示的图片名称
             }
-            else if (strcmp(current_bmp, "./2.bmp") == 0) //如果当前显示的是杂食.bmp
+            else if (strcmp(current_bmp, "./杂食.bmp") == 0) //如果当前显示的是杂食.bmp
             {
-                show_bmp("./1.bmp"); //显示菜单.bmp
-                current_bmp = "./1.bmp"; //更新当前显示的图片名称
+                show_bmp("./菜单.bmp"); //显示菜单.bmp
+                current_bmp = "./菜单.bmp"; //更新当前显示的图片名称
             }
         }
-        //TODO: 添加其他判断条件和操作，例如点击其他区域或商品时的处理逻辑
+        else if (is_tableware_clicked()) //如果用户点击了"餐具"
+        {
+            if (strcmp(current_bmp, "./菜单.bmp") == 0) //如果当前显示的是菜单.bmp
+            {
+                show_bmp("./餐具.bmp"); //显示餐具.bmp
+                current_bmp = "./餐具.bmp"; //更新当前显示的图片名称
+            }
+            else if (strcmp(current_bmp, "./餐具.bmp") == 0) //如果当前显示的是餐具.bmp
+            {
+                show_bmp("./菜单.bmp"); //显示菜单.bmp
+                current_bmp = "./菜单.bmp"; //更新当前显示的图片名称
+            }
+        }
+        else if (is_tool_clicked()) //如果用户点击了"用具"
+        {
+            if (strcmp(current_bmp, "./菜单.bmp") == 0) //如果当前显示的是菜单.bmp
+            {
+                show_bmp("./用具.bmp"); //显示用具.bmp
+                current_bmp = "./用具.bmp"; //更新当前显示的图片名称
+            }
+            else if (strcmp(current_bmp, "./用具.bmp") == 0) //如果当前显示的是用具.bmp
+            {
+                show_bmp("./菜单.bmp"); //显示菜单.bmp
+                current_bmp = "./菜单.bmp"; //更新当前显示的图片名称
+            }
+        }
+        else if (is_checkout_clicked()) //如果用户点击了"结算"
+        {
+            if (strcmp(current_bmp, "./菜单.bmp") == 0) //如果当前显示的是菜单.bmp
+            {
+                show_bmp("./结算.bmp"); //显示结算.bmp
+                current_bmp = "./结算.bmp"; //更新当前显示的图片名称
+            }
+            else if (strcmp(current_bmp, "./结算.bmp") == 0) //如果当前显示的是结算.bmp
+            {
+                show_bmp("./菜单.bmp"); //显示菜单.bmp
+                current_bmp = "./菜单.bmp"; //更新当前显示的图片名称 
+            }
+        }
     }
 }
